d07e04e1e0035bc3b69a36ded921f337778cb1bb
hyebinpark <hoonywhitesocks@gmail.com>
2025-08-26 16:24:25 +0900

    feat(reducer): cartReducer와 menuReducer 추가

--- src/redux/redux.js
+++ src/redux/redux.js
@@ -1,4 +1,7 @@
-// action
+// Action
+
+import data from "../assets/data";
+
 // 장바구니에 아이템 추가 (OrderModal.jsx 참고)
 const addToCart = (options, quantity, id) => {
   return {
@@ -6,6 +9,8 @@ const addToCart = (options, quantity, id) => {
     payload: { options, quantity, id },
   };
 };
+// - payload: { options, quantity, id } 형태로 전달
+// - 추후 price, name 같은 속성을 추가해도 구조 유지 가능
 
 // 장바구니 아이템 삭제 (Cart.jsx 참고)
 const removeFromCart = (id) => {
@@ -14,7 +19,27 @@ const removeFromCart = (id) => {
     payload: { id },
   };
 };
+// - payload: { id } → 해당 id와 다른 아이템만 남기도록 filter 처리 진행
 
-// reducer
+// Reducer
+// cartReducer: cart 상태 관리
+const cartReducer = (state = [], action) => {
+  // 초기값 빈 배열
+  switch (action.type) {
+    case "addToCart": // 기존 state에 새로운 아이템 추가
+      return [...state, action.payload];
+    case "removeFromCart": // id가 일치하지 않는 요소만 남김
+      return state.filter((el) => action.payload.id !== el.id);
+    default:
+      return state;
+  }
+};
+
+// menuReducer: menu 상태 관리
+const menuReducer = (state = data.menu, action) => {
+  // 초기값 data.menu
+  return state;
+};
+
 // dispatch
 // store
